95d6bb1b7b4b0b2622b935a79a6c402343fe5503
nitinkumargiri <nitingirinitingiri240@gmail.com>
2026-01-04 19:35:03 +0530

    bot

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,30 @@
-print("WELCOME TO THE PYTHON CHATBOT")
+
+print(" THE PWELCOME TOYTHON CHATBOT")
+
+print ("you enter bye or exit to end it.")
+
+data = {
+    "hi":"hi,what's going on","who are you":"i am a smart chatbot",
+    'who':'i am a chatbot',"how are you":"i am fine, thank you","what is your name":"i am called chatbot",
+    }
+
+def my_function(user_question):
+    user_question = user_question.lower()
+    for eachkey in data:
+        if eachkey in user_question:
+            return data[eachkey]
+        
+        return "i am not able to tell that yet, i am still in learning mode"
+    
+while True:
+
+        userinput = input("you: ")
+        reply = my_function(userinput)
+
+        print("Bot: ",reply)
+
+        if "bye" in userinput or "exit" in userinput.lower():
+            print ("Bot : bye have a nice day..!")
+            break
+
+
