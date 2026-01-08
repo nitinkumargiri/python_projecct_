0add34247b16595cc6ab49bad6351152713dbbb2
nitinkumargiri <nitingirinitingiri240@gmail.com>
2026-01-08 20:28:39 +0530

    simple

---
+++ hi.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    print("hello " + name)
+    
+persion1 = {
+    "namme": "nitingiri",
+    "age": 18,
+    "class": "dip"
+}
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,13 @@
-    print("your generated password is : ",password) 
+from hi import persion1
+int(persion1["age"])
+# import platform
+# x =hi.persion1['age']
+# print(x)
+# x = platform.system()
+# print(x)
+x = 20
+y = 4
+z = 3
+tex = "this is {1} this is {1} this is {2}"
+print (tex.format(x,y,z))
+print ("hello")
