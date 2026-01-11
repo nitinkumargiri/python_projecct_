99c4867a3d263038a9ba92a63b45f57bfa269b8b
nitinkumargiri <nitingirinitingiri240@gmail.com>
2026-01-11 19:35:48 +0530

    otp_generator

--- otp_generator.py
+++ otp_generator.py
@@ -2,4 +2,17 @@ import random
 import time
 
 otp = random.randint(100000,999999)
+expiry_time = 10
+
 print("your otp is : ",otp)
+start_time = time.time() 
+
+user_otp = input("enter otp withen 10 second: ")
+end_time = time.time()
+
+if end_time - start_time > expiry_time:
+    print("otp is expired❌❌")
+elif user_otp == str(otp):
+    print("otp enter sucessfully✅✅✅")
+else:
+    print("Invailed otp..!❌❌❌❌❌⚔️⚔️")
